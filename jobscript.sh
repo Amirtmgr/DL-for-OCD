e526cabccb527b915ff3037b3cf8078b10743082
amirtmgr <amirmagarmail1@gmail.com>
2023-09-18 18:14:21 +0200

    Update jobscript.

--- jobscript.sh
+++ jobscript.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=gpu           # partition to run in
 #SBATCH --nodes=1                   # node count
 #SBATCH --ntasks-per-node=64         # total number of tasks across all nodes<
-#SBATCH --time=24:00:00             # total run time limit (HH:MM:SS)
-#SBATCH --output=cluster_%j.%x.out  # where to save the output ( %j = JobID, %x = scriptname)
+#SBATCH --time=1:00:00             # total run time limit (HH:MM:SS)
+#SBATCH --output=$HOME/thesis/logs/cluster_%j.%x.out  # where to save the output ( %j = JobID, %x = scriptname)
 #SBATCH --gres gpu:1              # Generic resource required (1/2/4)
 #SBATCH --mail-type=ALL             # send all email
 #SBATCH --mail-user=$EMAIL          # email address from environemnt variable $EMAIL
+#SBATCH --error=$HOME/thesis/err/slurm.%j.err       # where to save error messages ( %j = JobID)
 
 #Optional parameters
 ##SBATCH --mem=100	                # 100 MB RAM per allocated CPU
-##SBATCH --error=slurm.%j.err       # where to save error messages ( %j = JobID)
 
 
 
