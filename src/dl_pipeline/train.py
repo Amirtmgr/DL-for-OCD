c0ea088febf62a88325089f1783d3205b64e6ea2
amirtmgr <amirmagarmail1@gmail.com>
2023-09-24 20:29:05 +0200

    Weighted BCELoss fixed.

--- src/dl_pipeline/train.py
+++ src/dl_pipeline/train.py
@@ -344,13 +344,14 @@ def load_optim(model):
 def load_criterion(weights):
     loss = cl.config.criterion.name
 
-    if cl.config.criterion.weighted:
+    if cl.config.criterion.weighted and weights is not None:
         class_weights = weights.to(cl.config.train.device)
         if loss == 'cross_entropy':
             criterion = nn.CrossEntropyLoss(weight=class_weights)
             Logger.info(f"Using CrossEntropyLoss with class weights: {class_weights}")
         elif loss == 'bce':
-            criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights)
+            # Pass positive class weight only
+            criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights[1])
             Logger.info(f"Using BCEWithLogitsLoss with class weights: {class_weights}")
     else:
         if cl.config.dataset.num_classes == 2:
