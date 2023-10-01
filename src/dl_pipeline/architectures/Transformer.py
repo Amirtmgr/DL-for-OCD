544daa15698d17734211ee6f39f20c1ce5144fec
Amir Thapa Magar <amir.thapamagar@student.uni-siegen.de>
2023-10-01 02:18:33 +0000

    Update import fix

--- src/dl_pipeline/architectures/Transformer.py
+++ src/dl_pipeline/architectures/Transformer.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
-
+import numpy as np
+from src.helper.logger import Logger
 
 class CNNTransformer(nn.Module):
 
