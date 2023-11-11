7ed9f5f5fea34b251e54b65e4469457571560571
Amir Thapa Magar <amir.thapamagar@student.uni-siegen.de>
2023-11-11 12:34:01 +0000

    Test

--- src/dl_pipeline/dl.py
+++ src/dl_pipeline/dl.py
@@ -119,10 +119,14 @@ def train():
             v.subwise_k_fold_cv(device, multi_gpu)
         elif cv == "stratified":
             v.stratified_k_fold_cv(device, multi_gpu)
+            # TODO: Remove this
+            cl.config.optim.name = 'sgd'
             ratios = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
-            for r in ratios:
-                cl.config.dataset.train_ratio = r
-                p.run(device, multi_gpu)
+            for lr in [0.01, 0.001, 0.0001, 0.00005, 0.00001]:
+                cl.config.optim.learning_rate = lr
+                for r in ratios:
+                    cl.config.dataset.train_ratio = r
+                    p.run(device, multi_gpu)
         elif cv == "personalized":
             p.run(device, multi_gpu)
         else:
