365ab59aee87d716a9bb2c0ab5261562de91081b
Thapa Magar <g056127@hpc-login02.cm.cluster>
2023-12-20 01:35:01 +0100

    fix nodes

--- jobscripts/dl_scripts/sgd/task_02_dl.sh
+++ jobscripts/dl_scripts/sgd/task_02_dl.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=DL_S20         # name for your job
 #SBATCH --partition=gpu           # partition to run in
 #SBATCH --ntasks=1                  # total number of tasks across all nodes
-#SBATCH --ntasks-per-node=64        # total number of tasks across all nodes<
+#SBATCH --ntasks-per-node=16        # total number of tasks across all nodes<
 #SBATCH --time=24:00:00             # total run time limit (HH:MM:SS)
 #SBATCH --output=cluster/omni_%x_%j.out  # where to save the output ( %j = JobID, %x = scriptname)
 #SBATCH --error=cluster/omni_%x_%j.err       # where to save error messages ( %j = JobID)
