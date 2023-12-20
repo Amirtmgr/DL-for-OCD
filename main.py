86bf7ed55e085c224d7ce69dca43aa8132e2ddea
Thapa Magar <g056127@hpc-login02.cm.cluster>
2023-12-20 07:18:57 +0100

    updates

--- main.py
+++ main.py
@@ -199,6 +199,7 @@ def main():
         dl.train()
     elif args.method == 'ml':
         cl.config.dataset.name = "processed"
+        cl.config.train.cross_validation.name = "losocv"
         ml.run()
     elif args.method == 'lazy':
         cl.config.dataset.name = "processed"
